08992f6aa3473e17b493b1f82ae1b0b04e413bb3
John Lynch <johnlynch.32768@gmail.com>
2020-01-22 14:56:34 +0000

    Generalise and comment 'fade()' function.

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -4,18 +4,22 @@ document.onreadystatechange = _ => {
     fade(msgs);
   }
 };
-function fade(elems) {
+function fade(elems, delay = 4, interval = 0.1, step = 0.1) {
+  // delay (before fade starts) in seconds
+  // step (reduction in opacity per step)
+  // interval (time between steps) in seconds
   setTimeout(_ => { 
     [...elems].forEach(elem => {
       elem.style.opacity = 1; 
       const timer = setInterval(_ => {
-        if (elem.style.opacity == 0) {
+        if (elem.style.opacity <= 0) {
           clearInterval(timer);
+          elem.style.display = `none`;
         }
         else {
-          elem.style.opacity -= 0.1;
+          elem.style.opacity -= step;
         }
-      }, 100);
+      }, interval * 1000);
     });
-  }, 4000);
+  }, delay * 1000);
 }
